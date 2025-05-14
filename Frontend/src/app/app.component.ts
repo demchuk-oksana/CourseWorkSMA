c1cfb952a0d5871d3db8ad2d0447db15d7f9eb6b
deasana <demchuk.oksana.w@gmail.com>
2025-05-14 19:16:14 +0300

    .

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from './services/api.service';
-import { CommonModule } from '@angular/common'; // Add this import
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http'; // Import HttpClientModule
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [CommonModule], // Add this line
-  standalone: true // Add this line
+  imports: [CommonModule, HttpClientModule], // Add HttpClientModule here
+  standalone: true
 })
 export class AppComponent implements OnInit {
   exampleData: any;
