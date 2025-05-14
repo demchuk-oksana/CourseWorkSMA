dc110cf3ae584341aadad9c901be2fd4cfa8656b
deasana <demchuk.oksana.w@gmail.com>
2025-05-14 18:08:46 +0300

    fixed running the frontend

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from './services/api.service';
+import { CommonModule } from '@angular/common'; // Add this import
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  imports: [CommonModule], // Add this line
+  standalone: true // Add this line
 })
 export class AppComponent implements OnInit {
   exampleData: any;
