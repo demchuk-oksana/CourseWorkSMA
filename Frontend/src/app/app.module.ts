deeba57794ce3874c36c29925085e903d776baf8
deasana <demchuk.oksana.w@gmail.com>
2025-05-14 18:05:07 +0300

    changed app module

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,19 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common'; // Import CommonModule
 import { AppComponent } from './app.component';
 import { CategoryTreeComponent } from './category-tree/category-tree.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CategoryTreeComponent // Declare your components here
+    CategoryTreeComponent, // Declare your components here
   ],
   imports: [
     BrowserModule, // Provides Angular's core browser functionality
-    HttpClientModule // For making HTTP requests in services
+    HttpClientModule, // For making HTTP requests in services
+    CommonModule, // Add CommonModule here
   ],
   providers: [],
-  bootstrap: [AppComponent] // Bootstraps the AppComponent
+  bootstrap: [AppComponent], // Bootstraps the AppComponent
 })
-export class AppModule { }
+export class AppModule {}
